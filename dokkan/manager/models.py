68897ae82971ee473ce01498ed55554bb5b6dc71
Yuta Kimura <52063678+kyuta8@users.noreply.github.com>
2020-03-05 21:34:50 +0900

    update database
    データベースの更新

--- dokkan/manager/models.py
+++ dokkan/manager/models.py
@@ -51,7 +51,7 @@ class Manager(models.Model):
     DEP_IS = 50  # 情報システム
 
     # 人
-    person = models.ForeignKey('Person')
+    person = models.ForeignKey('Person', on_delete=models.CASCADE,)
     # 部署
     department = models.IntegerField()
     # 着任時期
@@ -63,10 +63,10 @@ class Manager(models.Model):
 class Worker(models.Model):
 
     # 人
-    person = models.ForeignKey('Person')
+    person = models.ForeignKey('Person', on_delete=models.CASCADE,)
     # 着任時期
     joined_at = models.DateTimeField()
     # やめた時期
     quited_at = models.DateTimeField(null=True, blank=True)
     # 担当上司
-    manager = models.ForeignKey('Manager')
+    manager = models.ForeignKey('Manager', on_delete=models.CASCADE,)
